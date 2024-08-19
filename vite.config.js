6167b45690d6406eac69bfd4fe74f7b05a614fd4
Chystiakov73Viktor <130795120+Chystiakov73Viktor@users.noreply.github.com>
2024-08-19 13:26:36 +0300

    project settings

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { envConfig } from 'vite-plugin-env-config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), envConfig()],
+  resolve: {
+    alias: {
+      src: '/src',
+      components: '/src/components',
+      pages: '/src/pages',
+      constants: '/src/constants',
+      images: '/src/images',
+    },
+  },
 })
